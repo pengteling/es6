d61e1fa8f8e8356c42928e81d9621d12729edcfa
pengteling <pengteling@gmail.com>
2016-12-05 14:43:53 +0800

    1205

--- dist/js/index.js
+++ dist/js/index.js
@@ -29,35 +29,38 @@
 // console.log(s);
 // console.log(s.has(5));
 
-var queuedObservers = new Set();
+// var queuedObservers = new Set();
 
-var observe = function observe(fn) {
-  return queuedObservers.add(fn);
-};
-var observable = function observable(obj) {
-  return new Proxy(obj, { set: set });
-};
+// var observe = function observe(fn) {
+//   return queuedObservers.add(fn);
+// };
+// var observable = function observable(obj) {
+//   return new Proxy(obj, { set: set });
+// };
 
-function set(target, key, value, receiver) {
-  var result = Reflect.set(target, key, value, receiver);
-  queuedObservers.forEach(function (observer) {
-    return observer();
-  });
-  return result;
-}
-var person = observable({
-  name: '张三',
-  age: 20
-});
+// function set(target, key, value, receiver) {
+//   var result = Reflect.set(target, key, value, receiver);
+//   queuedObservers.forEach(function (observer) {
+//     return observer();
+//   });
+//   return result;
+// }
+// var person = observable({
+//   name: '张三',
+//   age: 20
+// });
 
-function print() {
-  console.log(person.name + ', ' + person.age);
-}
-function sb() {
-  console.log('' + person.prototype);
-}
+// function print() {
+//   console.log(person.name + ', ' + person.age);
+// }
+// function sb() {
+//   console.log('' + person.prototype);
+// }
 
-observe(print);
-observe(sb);
-person.name = '李四';
-person.name = 'ptl';
+// observe(print);
+// observe(sb);
+// person.name = '李四';
+// person.name = 'ptl';
+
+
+let myMap = new Map().set(true, 7).set({foo: 3}, ['abc']);
